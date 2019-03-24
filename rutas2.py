b83508cb1a3438de2488e1eabc987e4019a57edd
Yeiduard <Ygonzalez689@unab.edu.co>
2019-03-24 00:31:51 -0500

    Update rutas2.py

--- rutas2.py
+++ rutas2.py
@@ -14,7 +14,7 @@ def index():
 	
     title = " Tu divisa"
 
-    return render_template("index.html", title=title)
+    return render_template("Index.html", title=title)
 
 
 @app.route('/Tabla_dolar')
@@ -25,10 +25,7 @@ def client():
     return render_template("pruebagraf.html", x=x, y=y)
 
 
-def sumar(val1,val2):
-	
-	total= val1+val2
-	return total
+
 
 
 app.run(debug=True, port=5000)
