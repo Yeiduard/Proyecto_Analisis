78c4aadf97827e431e2942b3107b07e4a3b03269
Yeiduard <48776253+Yeiduard@users.noreply.github.com>
2019-03-26 21:33:54 -0500

    Add files via upload

--- Tablas_de_divisas.py
+++ Tablas_de_divisas.py
@@ -4,25 +4,40 @@ from matplotlib import pyplot as plt
 from flask import Response
 import numpy as np
 import json
+import os
+import requests
+  
+r=requests.get('http://api.wahrungsrechner.org/v1/full/EUR/json?key=1881|hTayEgZ3xeee6SRdw3e~ZCvd9hmiA4iS')
+Usd=requests.get('http://api.wahrungsrechner.org/v1/full/USD/json?key=1881|hTayEgZ3xeee6SRdw3e~ZCvd9hmiA4iS')
+url = 'http://api.wahrungsrechner.org/v1/full/EUR/json?key={1881|hTayEgZ3xeee6SRdw3e~ZCvd9hmiA4iS}'
 
 
-url = 'https://www.datos.gov.co/resource/g3ab-sax9.json'
+#d=pd.read_json(r)
+respuesta = r.json()
+t2=Usd.json()
+#print(t2)
+rate = respuesta['result']['conversion'][30]['rate']
+UsdtoCop= t2['result']['conversion'][30]['rate']
+EuroDivisacolombiana=respuesta['result']['conversion'][30]['rate']
+UsdToEU=t2['result']['conversion'][43]['rate']
+
+print(EuroDivisacolombiana)
+#data = pd.read_csv('Tasa_representativa_del_Mercado.csv', header=0)
 
 
 
-data = pd.read_csv('Tasa_representativa_del_Mercado.csv', header=0)
 # print(data)
 
-x = data['VALOR']
-y = data['VIGENCIADESDE']
+#x = data['VALOR']
+#y = data['VIGENCIADESDE']
 
 
-plt.plot(x, y, 'ro')
-plt.xlabel('Valor del dolar')
-plt.ylabel('Vigencia')
+#plt.plot(x, y, 'ro')
+#plt.xlabel('Valor del dolar')
+#plt.ylabel('Vigencia')
 
 
-plt.show
+#plt.show
 
 
 #def listadoDolar():
--- rutas2.py
+++ rutas2.py
@@ -3,25 +3,28 @@ from flask import request
 from flask import render_template
 import Tablas_de_divisas as td
 import forms
-
+from flask import request
+from flask import jsonify
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    title = " Tu divisa"
-
-    return render_template("index.html", title=title)
+    #x=1881|hTayEgZ3xeee6SRdw3e~ZCvd9hmiA4iS
+    #r=requests.get('http://api.wahrungsrechner.org/v1/quotes/THB/EUR/json?qty=1&key={1881|hTayEgZ3xeee6SRdw3e~ZCvd9hmiA4iS}')
+    UsdtoCop=td.UsdtoCop
+    eurotoCol=td.EuroDivisacolombiana
+    usdtoEu=td.UsdToEU
+    return render_template("index.html",eurotoCol=eurotoCol,UsdtoCop=UsdtoCop,usdtoEu=usdtoEu)
 
 
 @app.route('/pruebagraf.html')
 def client():
 
-    x = td.x
-    y = td.y
-    return render_template("pruebagraf.html", x=x, y=y)
+    
+    return render_template("pruebagraf.html")
 
 
 @app.route('/inversion.html', methods=['GET', 'POST'])
